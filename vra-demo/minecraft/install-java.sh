0e505878dd296c5d9125051ddd6499fc32d8b372
prydin <prydin@vmware.com>
2020-05-14 11:37:43 -0400

    Changed version of oracle license

--- vra-demo/minecraft/install-java.sh
+++ vra-demo/minecraft/install-java.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo add-apt-repository -y ppa:linuxuprising/java
-echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-2 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-2 seen true | sudo debconf-set-selections
 sudo apt install -y openjdk-14-jre-headless 
 
