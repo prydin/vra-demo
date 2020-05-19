913caa7ba9572d9d61334046d3c8551987af7f9b
prydin <prydin@vmware.com>
2020-05-19 10:34:38 -0400

    More changed

--- vra-demo/minecraft/install-spigot.sh
+++ vra-demo/minecraft/install-spigot.sh
@@ -7,9 +7,13 @@ git config --global --unset core.autocrlf
 java -Xmx2G -jar BuildTools.jar
 echo "eula=true" > eula.txt 
 
-# Add seed if specified
+# Add settings if
+wget https://raw.githubusercontent.com/prydin/vra-demo/master/vra-demo/minecraft/server.properties
 if [ -n "$MC_SEED" ]; then
-    wget https://raw.githubusercontent.com/prydin/vra-demo/master/vra-demo/minecraft/server.properties
     sed -ibak "s/\$MC_SEED/$MC_SEED/" server.properties
+fi
+
+if [ -n "$MC_GAMEMODE" ]; then
+    sed -ibak "s/gamemode=survival/gamemode=$MC_GAMEMODE/" server.properties
 fi
 
