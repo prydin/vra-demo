472887689fbb6fe45898b9b88ca3b4f683abc72f
prydin <prydin@vmware.com>
2020-05-20 10:55:06 -0400

    Better service definition file

--- minecraft/install-spigot.sh
+++ minecraft/install-spigot.sh
@@ -8,6 +8,7 @@ wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifac
 git config --global --unset core.autocrlf
 java -Xmx2G -jar BuildTools.jar
 echo "eula=true" > eula.txt 
+ln -s spigot-$MC_VERSION.jar spigot.jar
 
 # Add game settings
 sed -i.bak "s/\$MC_SEED/$MC_SEED/" server.properties
@@ -20,6 +21,10 @@ sed -i.bak "s/\$MC_GAMEMODE/$MC_GAMEMODE/" server.properties
 
 # Create log directory
 mkdir /opt/spigot/log
+
+# Make minecraft the owner of it all
+chown -R minecraft .
+chgrp -R minecraft .
 
 # Install service
 cp /opt/spigot/spigot.service /etc/systemd/system
